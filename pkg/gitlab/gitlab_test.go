92ab7f2ca64e0c174a3b2a8f2bc825fce09ea07c
mikeskaife <mike@skaife.me>
2020-06-08 17:45:39 +0100

    simple test for Pipeline event with data

--- pkg/gitlab/gitlab_test.go
+++ pkg/gitlab/gitlab_test.go
@@ -24,14 +24,14 @@ func TestMain(m *testing.M) {
 	// teardown
 }
 
-func TestGitLabHooks(t *testing.T) {
+func TestGitLabHookHeaders(t *testing.T) {
 
 	assert := require.New(t)
 
 	tests := []struct {
-		name     string
-		headers  http.Header
-		response int
+		name         string
+		headers      http.Header
+		responseCode int
 	}{
 		{
 			"GitLab hook received with no event header",
@@ -41,7 +41,7 @@ func TestGitLabHooks(t *testing.T) {
 		{
 			"GitLab hook received with valid event header for Pipeline event",
 			http.Header{
-				gitlab.HookHeader: []string{"Pipeline Hook"},
+				gitlab.HookHeader: []string{gitlab.PipelineEvent},
 			},
 			http.StatusOK,
 		},
@@ -57,8 +57,6 @@ func TestGitLabHooks(t *testing.T) {
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 
-			//t.Parallel()
-
 			req, err := http.NewRequest(http.MethodPost, "/hook/gitlab", nil)
 			assert.NoError(err)
 
@@ -66,7 +64,44 @@ func TestGitLabHooks(t *testing.T) {
 
 			resp := executeRequest(req)
 
-			assert.Equal(tt.response, resp.Code)
+			assert.Equal(tt.responseCode, resp.Code)
+		})
+	}
+}
+
+func TestGitLabHookPipelineEvent(t *testing.T) {
+	assert := require.New(t)
+
+	tests := []struct {
+		name         string
+		headers      http.Header
+		dataPath     string
+		responseCode int
+	}{
+		{
+			"valid GitLab pipeline event",
+			http.Header{
+				gitlab.HookHeader: []string{gitlab.PipelineEvent},
+			},
+			"./testdata/pipelineEvent.json",
+			http.StatusOK,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+
+			data, err := os.Open(tt.dataPath)
+			assert.NoError(err)
+
+			req, err := http.NewRequest(http.MethodPost, "/hook/gitlab", data)
+			assert.NoError(err)
+
+			req.Header = tt.headers
+
+			resp := executeRequest(req)
+
+			assert.Equal(tt.responseCode, resp.Code)
 		})
 	}
 }
